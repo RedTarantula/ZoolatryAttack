5948e1504648f06386c1f93b13df577055c6cb11
RedTarantula <joaopedrothmann@hotmail.com>
2020-03-31 12:06:12 -0300

    Pickups prefabs and placeholders

---
+++ ZoolatryAttack/Assets/AlwaysRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlwaysRotate : MonoBehaviour
+{
+    public Vector3 rotateSpeed;
+
+    private void Update()
+    {
+        transform.Rotate(rotateSpeed*Time.deltaTime);
+    }
+}
