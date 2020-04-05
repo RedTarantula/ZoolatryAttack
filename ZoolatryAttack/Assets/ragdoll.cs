106f0439823a9392084cfdfe03ca3f27db371b69
freitaswill <willianfreitas00@gmail.com>
2020-04-05 18:10:06 -0300

    Ragdoll Implementation and Photon Setup

---
+++ ZoolatryAttack/Assets/ragdoll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ragdoll : MonoBehaviour
+{
+    public bool ragdollBool = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetKinematic(ragdollBool);
+    }
+
+    void SetKinematic(bool newValue)
+    {
+        Rigidbody[] bodies = GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody rb in bodies)
+        {
+            rb.isKinematic = newValue;
+        }
+    }
+}
